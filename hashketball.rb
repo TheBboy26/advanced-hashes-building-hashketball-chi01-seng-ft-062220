1aba89b7e67073cb9313f8a8e6e0ce27d26bbd5a
Josue Viramontes <josueviramontes1997@gmail.com>
2020-06-19 15:55:32 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -19,12 +19,12 @@ def game_hash
           player_name: "Reggie Evans",
           number: 30,
           shoe: 14,
-          points: 12,
-          rebounds: 12,
-          assists: 12,
-          steals: 12,
-          blocks: 12,
-          slam_dunks: 7
+         points: 12,
+         rebounds: 12,
+        assists: 12,
+        steals: 12,
+         blocks: 12,
+         slam_dunks: 7
         },
         {
          player_name: "Brook Lopez",
@@ -81,19 +81,19 @@ def game_hash
           number: 0,
           shoe: 16,
           points: 12,
-          rebounds: 4,
-          assists: 7,
+        rebounds: 4,
+         assists: 7,
           steals: 22,
-          blocks: 15,
-          slam_dunks: 10
+         blocks: 15,
+         slam_dunks: 10
         },
         {
           player_name: "DeSagna Diop",
-          number: 2,
+         number: 2,
           shoe: 14,
-          points: 24,
+         points: 24,
           rebounds: 12,
-          assists: 12,
+         assists: 12,
           steals: 4,
           blocks: 5,
           slam_dunks: 5
@@ -101,22 +101,22 @@ def game_hash
         {
           player_name: "Ben Gordon",
           number: 8,
-          shoe: 15,
+        shoe: 15,
           points: 33,
-          rebounds: 3,
+        rebounds: 3,
           assists: 2,
           steals: 1,
-          blocks: 1,
+         blocks: 1,
           slam_dunks: 0
         },
         {
           player_name: "Kemba Walker",
           number: 33,
-          shoe: 15,
+        shoe: 15,
           points: 6,
-          rebounds: 12,
+         rebounds: 12,
           assists: 12,
-          steals: 7,
+         steals: 7,
           blocks: 5,
           slam_dunks: 12
         }
